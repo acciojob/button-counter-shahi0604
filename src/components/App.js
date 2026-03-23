79389fbe317374301ebde176c0c577c6fed34b2a
Shahina <166979082+shahi0604@users.noreply.github.com>
2026-03-23 18:12:05 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,11 @@
 
 import React from "react";
-import ButtonCounter from "./ButtonCounter";
 import './../styles/App.css';
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
-      <ButtonCounter />
     </div>
   )
 }
